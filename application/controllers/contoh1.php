118b5a49c15dc833db1420ab92793629ad67a0a5
leokandaprayoga <leokandaprayoga@gmail.com>
2024-03-29 13:00:25 +0700

    tugas 1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Leo kanda prayoga. Saya tinggal di Pontianak. Olahraga yang saya sukai adalah Bulutangkis";
+    }
+}
+?>
