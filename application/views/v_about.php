e9adff700344cef1d86c7641fa74af1d29d33984
leokandaprayoga <leokandaprayoga@gmail.com>
2024-04-25 15:57:40 +0700

    tugas 3 pertemuan 4

---
+++ application/views/v_about.php
@@ -0,0 +1,29 @@
+<section>
+    <h1><?php echo $judul?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama depan : Leo</li>
+        <li>Nama Belakang : Kanda Prayoga</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="circle">
+        <li>Sepakat II gg.paderang</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="circle">
+        <li>Badau</li>
+    </ul>
+
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Sepakat II gg.paderang</li>
+    </ul>
+
+    <h4>Olahraga favorit</h4>
+    <ul type="squere">
+        <li>Bulu Tangkis</li>
+    </ul>
+</section>
+
---
+++ application/views/v_header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Web prog II | merancang Tamplate sederhana dengan codeigniter</title>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/stylebuku.css'); ?>">
+
+</head>
+<body>
+    <div id= "wrapper">
+        <header>
+            <hgroup>
+                <h1>RentalBuku.Net</h1>
+                <h3>Membuat Tamplate Sederhana dengan CodeIgniter</h3>
+            </hgroup>
+            <nav>
+                <ul>
+                    <li><a href="<?php echo base_url() . 'web' ?>">Home</a><li>
+                    <li><a href="<?php echo base_url() . 'web/about' ?>">About</a><li>
+                </ul>
+            </nav>
+            <div class="clear"></div>
+        </header>
