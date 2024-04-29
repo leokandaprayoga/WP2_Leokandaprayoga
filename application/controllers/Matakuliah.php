49ef563e8c68c47d80cefa5113778848a72e4f17
leokandaprayoga <leokandaprayoga@gmail.com>
2024-04-29 08:00:42 +0700

    Pertemuan 5 Tugas 4

--- application/controllers/Matakuliah.php
+++ application/controllers/Matakuliah.php
@@ -8,12 +8,33 @@ class Matakuliah extends CI_Controller
 
     public function cetak()
     {
-        $data = [
-            'kode' => $this->input->post('kode'),
-            'nama' => $this->input->post('nama'),
-            'sks' => $this->input->post('sks'),
-        ];
+        $this->form_validation->set_rules('kode','kode Matakuliah',
+            'required|min_length[3]',[
+            'required'=>'kode Matakuliah harus diisi',
+            'min_length'=>'kode terlalu pendek']);
 
-        $this->load->view('View_data_matakuliah', $data);
+        $this->form_validation->set_rules('nama','nama Matakuliah',
+            'required|min_length[3]',[
+            'required'=>'Nama Matakuliah harus diisi',
+            'min_length'=>'Nama Matakuliah terlalu pendek']);
+
+        $this->form_validation->set_rules('sks','SKS',
+        'required', [
+            'required'=>'SKS harus diisi'
+        ]);
+
+        // Jalankan validasi
+        if ($this->form_validation->run() != true){
+            $this->load->view('View_form_matakuliah');
+        } else {
+            $data = [
+                'kode' => $this->input->post('kode'),
+                'nama' => $this->input->post('nama'),
+                'sks' => $this->input->post('sks'),
+            ];
+    
+            $this->load->view('View_data_matakuliah', $data);
+        }     
     }
 }
+?>
--- application/views/View_form_matakuliah.php
+++ application/views/View_form_matakuliah.php
@@ -23,13 +23,15 @@
                 <th>:</th>
                 <td>
                     <input type="text" name="kode" id="kode" placeholder="Input Kode MTK">
+                    <div class="invalid-feedback"><?=form_error('kode')?></div>
                 </td>
             </tr>
             <tr>
                 <th>Nama MTK</th>
                 <td>:</td>
                 <td>
-                    <input type="text" name="nama" id="kode" placeholder="Input Nama MTK">  
+                    <input type="text" name="nama" id="nama" placeholder="Input Nama MTK">
+                    <div class="invalid-feedback"><?=form_error('nama')?></div>  
                 </td>
             </tr>
             <tr> 
@@ -42,6 +44,7 @@
                         <option value="3">3</option>
                         <option value="4">4</option>
                     </select>
+                    <div class="invalid-feedback"><?=form_error('sks')?></div> 
                 </td>
             </tr>
             <tr>
